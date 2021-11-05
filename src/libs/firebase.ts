db5d1085c4c229a15d69e4e766f19cdcb819c9da
AlexMarqueAlves <atanaelleonardo@gmail.com>
2021-11-05 18:56:09 -0300

    cleaning app.tsx

--- src/libs/firebase.ts
+++ src/libs/firebase.ts
@@ -1,17 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB_Gr-2qKBXrl35eLOJEbGM3n_fUnyqAzQ",
-  authDomain: "d5reactgallery-9f63c.firebaseapp.com",
-  projectId: "d5reactgallery-9f63c",
-  storageBucket: "d5reactgallery-9f63c.appspot.com",
-  messagingSenderId: "330859891133",
-  appId: "1:330859891133:web:f27cfe3a1940179b646ad4"
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSEAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
+
+export const storage = getStorage(firebaseApp);
