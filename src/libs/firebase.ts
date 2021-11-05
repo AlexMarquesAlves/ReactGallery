6fd5994780fa1774010469c1dace48e594e261a1
AlexMarqueAlves <atanaelleonardo@gmail.com>
2021-11-05 17:49:42 -0300

    creating firebase

---
+++ src/libs/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB_Gr-2qKBXrl35eLOJEbGM3n_fUnyqAzQ",
+  authDomain: "d5reactgallery-9f63c.firebaseapp.com",
+  projectId: "d5reactgallery-9f63c",
+  storageBucket: "d5reactgallery-9f63c.appspot.com",
+  messagingSenderId: "330859891133",
+  appId: "1:330859891133:web:f27cfe3a1940179b646ad4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
