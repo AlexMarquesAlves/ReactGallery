849fd35664fb656ad91b812487b58ed6083581ee
AlexMarqueAlves <atanaelleonardo@gmail.com>
2021-11-06 15:51:09 -0300

    estilizando a pagina...

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,3 +1,20 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  background-color: #27282f;
+  color: #fff;
+  min-height: 100vh;
+`;
+
+export const Area = styled.div`
+  margin: auto;
+  max-width: 980px;
+  padding: 30px 0;
+`;
+
+export const Header = styled.h1`
+  margin: 0;
+  padding: 0;
+  text-align: center;
+  margin-bottom: 30px;
+`;
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import * as C from './App.styles';
+
+const App = () => {
+  return (
+    <C.Container>
+      <C.Area>
+        <C.Header>Galeria de Fotos</C.Header>
+
+        {/* Área de upload */}
+
+        {/* lista de fotos */}
+      </C.Area>
+    </C.Container>
+  )
+}
+
+export default App;
