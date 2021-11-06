b5b7cf4f1457bf06255ff0bee48656d72f86af50
AlexMarqueAlves <atanaelleonardo@gmail.com>
2021-11-06 16:56:34 -0300

    update service firebase

--- src/services/photos.ts
+++ src/services/photos.ts
@@ -1,7 +1,6 @@
 import { Photo } from '../types/Photo';
 import { storage } from '../libs/firebase';
 import { ref, listAll, getDownloadURL } from 'firebase/storage';
-import { url } from 'inspector';
 
 export const getAll = async () => {
   let list: Photo[] = [];
