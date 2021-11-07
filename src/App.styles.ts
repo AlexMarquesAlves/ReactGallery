c14ff065ecdb17d686d7ed515483f45ecc57104c
AlexMarqueAlves <atanaelleonardo@gmail.com>
2021-11-06 21:31:35 -0300

    Tela de Loading...

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -18,3 +18,19 @@ export const Header = styled.h1`
   text-align: center;
   margin-bottom: 30px;
 `;
+
+export const ScreenWarning = styled.div`
+  text-align: center;
+
+  .emoji {
+    font-size: 100px;
+    margin-bottom: 20px;
+
+  }
+`;
+
+export const PhotoList = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4,1fr);
+  gap: 10px;
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
+import { useEffect, useState } from 'react';
 import * as C from './App.styles';
+import * as Photos from './services/photos';
+import { Photo } from './types/Photo';
 
 const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState<Photo[]>([]);
+
+  useEffect(() => {
+    const getPhotos = async () => {
+      setLoading(true);
+      setPhotos(await Photos.getAll());
+      setLoading(false);
+    }
+    getPhotos();
+  }, [])
+
   return (
     <C.Container>
       <C.Area>
@@ -8,7 +23,20 @@ const App = () => {
 
         {/* Área de upload */}
 
-        {/* lista de fotos */}
+        {loading &&
+          <C.ScreenWarning>
+            <div className="emoji">✋</div>
+            <div>Carregando...</div>
+          </C.ScreenWarning>
+        }
+
+        {!loading && photos.length > 0 &&
+          <C.PhotoList>
+            {photos.map((item, index) => {
+              <div>{item.name}</div>
+            })}
+          </C.PhotoList>
+        }
 
       </C.Area>
     </C.Container>
